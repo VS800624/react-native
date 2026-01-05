9cc4c4bd7a136645d1e4d6b1090bd0e15ac573cf
VS800624 <vishalsingh800624@gmail.com>
2026-01-05 12:13:47 +0530

    Initial Expo setup

---
+++ react-native-1/app/index.js
@@ -0,0 +1,27 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen. Hello</Text>
+      <Text>Hi</Text>
+      <Link href={"/about"}>Visit about screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,    
+        // flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+      },
+      content: {
+        fontSize: 30,
+      }
+})
