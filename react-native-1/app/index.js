3b80eeffae7b073be568b7da7673a4985c70a2c4
VS800624 <vishalsingh800624@gmail.com>
2026-01-05 19:27:13 +0530

    Learn about Modal component

--- react-native-1/app/index.js
+++ react-native-1/app/index.js
@@ -1,4 +1,5 @@
 import { Link } from "expo-router";
+import { useState } from "react";
 import {
   ActivityIndicator,
   StyleSheet,
@@ -9,17 +10,18 @@ import {
   ScrollView,
   Button,
   Pressable,
+  Modal,
 } from "react-native";
 const logoImg = require("../assets/images/react-logo.png");
 
 export default function Index() {
+  const [isVisible, setIsVisible] = useState(false)
   return (
     <View style={styles.container}>
       <ScrollView>
         <Pressable onPress={() => console.log("Image pressed")}>
           <Image source={logoImg} style={{width:300, height: 300}}/>
         </Pressable>
-        {/* <Image source={{uri:"https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8="}} style={{width:300, height: 300}}/> */}
         {/* <ImageBackground source={logoImg}>
           <Text>Hello</Text>
           </ImageBackground> */}
@@ -27,9 +29,17 @@ export default function Index() {
             <Text style={styles.content}> Hello World</Text>
           </Pressable>
         <Link href={"/about"}>Visit about screen</Link>
-        <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptas cum, soluta doloribus repellendus impedit obcaecati blanditiis. Autem fugit soluta perspiciatis omnis aliquid possimus atque, doloribus molestiae sequi deserunt beatae, modi odit maxime tenetur recusandae! Minus sed alias vero eaque vitae, veritatis atque repellat? Magni repellat vel facilis? Quod laborum perferendis numquam facilis in quisquam vitae adipisci eaque quos eum? Reiciendis odit, aperiam omnis, magni dolores dicta ex eveniet aliquam numquam fugiat repellat dignissimos quasi laboriosam quod ratione modi natus deserunt mollitia architecto. Molestias ipsa est repudiandae ad? At vel delectus repellendus labore odit accusamus id, ad ab velit fugiat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos hic nostrum amet voluptatum sit, reiciendis soluta dicta incidunt nesciunt quis suscipit animi itaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusamus impedit vel blanditiis voluptate laboriosam, debitis unde at sint veritatis!</Text>
-        <Button title="press" onPress={() => console.log("Button pressed")} color="green"
+        <Button title="press" onPress={() => setIsVisible(true)} color="green"
         />
+       <Modal visible={isVisible} onRequestClose={() => setIsVisible(false)} animationType="slide" presentationStyle="pageSheet">
+        {/* presentationStyle only effect IOS not the android */}
+        {/* onRequestClose is trigger when user press the back button on android or dismisses the modal with gesture in ios */}
+        <View style={styles.container}>
+          <Text>Modal</Text>
+           <Image source={{uri:"https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8="}} style={{width:300, height: 300}}/>
+          <Button title="close" color="midnightblue" onPress={() => setIsVisible(false)}/>
+        </View>
+       </Modal>
       </ScrollView>
     </View>
   );
